1f18be7bd88f0fe945660f602461b09b90c59506
Jered Masters <jered.masters@outlook.com>
2018-03-22 21:11:19 +0800

    configuring map

--- src/containers/Map/index.js
+++ src/containers/Map/index.js
@@ -6,7 +6,7 @@ const MyMapComponent = compose(
   withProps({
     googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyA_VUGh3U4g-hm-Oqw_esFeiqO2sf8QY9w&v=3.exp&libraries=geometry,drawing,places",
     loadingElement: <div style={{ height: `100%` }} />,
-    containerElement: <div style={{ height: `400px` }} />,
+    containerElement: <div style={{ height: `800px` }} />,
     mapElement: <div style={{ height: `100%` }} />,
   }),
   withScriptjs,
